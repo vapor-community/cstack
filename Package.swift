6de084f98bc9c7ac22bf5d2a12d4888b6add97f0
Tanner Nelson <me@tanner.xyz>
2017-08-09 22:50:12 -0400

    first commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CStack",
+    pkgConfig: "cstack",
+    providers: [
+        .brew(["vapor/tap/cstack"]),
+        .apt(["cstack"])
+    ]
+)
